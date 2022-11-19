bb9cad480bc505c97c24bc12ded7bac52819c2f1
aboutJulian <bloennigen.julian@gmail.com>
2022-11-19 18:21:42 +0100

    Update Cooking_Lasagna.py

--- Cooking_Lasagna.py
+++ Cooking_Lasagna.py
@@ -1,14 +1,19 @@
 EXPECTED_MNINUTES_IN_OVEN = 40
 
 def remainingMinutesInOven(TIME_ALREADY_IN_OVEN):
+    TIME_ALREADY_IN_OVEN = int(TIME_ALREADY_IN_OVEN)
     return EXPECTED_MNINUTES_IN_OVEN-TIME_ALREADY_IN_OVEN
 
 def preparationTimeInMinutes(NUMBER_OF_LAYERS):
+    NUMBER_OF_LAYERS = int(NUMBER_OF_LAYERS)
     return NUMBER_OF_LAYERS*2
 
 def totalTimeInMinutes(numberOfLayers, actualMinutesInOven):
-    return remainingMinutesInOven(actualMinutesInOven) + preparationTimeInMinutes(numberOfLayers)
+    return int(actualMinutesInOven) + preparationTimeInMinutes(numberOfLayers)
 
-print(totalTimeInMinutes(3, 20))
+user_layers = input("How many layers are in your lasagna? ")
+user_minutesInOven = input("How many minutes has your lasagna been in the oven? ")
+
+print("You have spent " + str(totalTimeInMinutes(user_layers, user_minutesInOven)) + " minutes on making your lasagna.")
 
 
